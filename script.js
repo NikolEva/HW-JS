79df1d28a805bb3066602a65c37ed732d3baf827
NikolEva <Alyona.nikolaeva.88@inbox.ru>
2023-01-22 18:50:16 +0300

    1hw

---
+++ script.js
@@ -0,0 +1,7 @@
+let tempCelcium = 10;
+let tempPhareng = (9 / 5) * tempCelcium + 32;
+console.log(`Температура в Цельсиях = ${tempCelcium} Температура в Фаренгейтах = ${tempPhareng}`);
+
+let name = "Alena";
+let admin = name;
+console.log(admin);
